6f84741d30941d89dfdc61f77fc99405bf491eeb
Bulat Zamilov <bulat.zamilov@gmail.com>
2019-04-17 23:16:29 +0300

    Add pipenv config

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 import os
+
 from flask import Flask, current_app, send_file
 
-app = Flask(__name__, static_folder='../dist/static')
+app = Flask(__name__, static_folder='static')
 
 
 @app.route('/')
 def default_route():
-    dist_dir = current.app.config['DIST_DIR']
+    dist_dir = current_app.config['DIST_DIR']
     entry = os.path.join(dist_dir, 'index.html')
 
     return send_file(entry)
+
+
+if __name__ == '__main__':
+    app.run()
