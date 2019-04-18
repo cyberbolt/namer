7cbec7ee7a8ee2d16179562f39b2ddf106c6217d
Bulat Zamilov <bulat.zamilov@gmail.com>
2019-04-18 23:09:06 +0300

    Make Flask + Vue scaffold run

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,26 @@
 import os
+from flask import Flask, current_app, send_file, send_from_directory
 
-from flask import Flask, current_app, send_file
+from .api import api_bp
+from .client import client_bp
 
-app = Flask(__name__, static_folder='static')
+
+app: Flask = Flask(__name__, static_folder='../dist/static')
+app.register_blueprint(api_bp)
+# app.register_blueprint(client_bp)
+
+from .config import Config
+app.logger.info('>>> {}'.format(Config.FLASK_ENV))
 
 
 @app.route('/')
-def default_route():
+def index_client():
     dist_dir = current_app.config['DIST_DIR']
     entry = os.path.join(dist_dir, 'index.html')
-
     return send_file(entry)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/favicon.ico')
+def favicon():
+    dist_dir = current_app.config['DIST_DIR']
+    return send_from_directory(dist_dir, 'favicon.ico', mimetype='image/vnd.microsoft.icon')
