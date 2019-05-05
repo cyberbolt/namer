b3139febfc5dfd1a50203bea52309684a2be66a4
Bulat Zamilov <bulat.zamilov@gmail.com>
2019-05-06 01:34:32 +0300

    Add connect to mongoDB

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,40 @@
 import os
+import json
+import datetime
+from bson.objectid import ObjectId
 from flask import Flask, current_app, send_file, send_from_directory
+from flask_pymongo import PyMongo
 
 from .api import api_bp
 from .client import client_bp
 
 
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
 app: Flask = Flask(__name__, static_folder='../dist/static')
 app.register_blueprint(api_bp)
-# app.register_blueprint(client_bp)
 
 from .config import Config
 app.logger.info('>>> {}'.format(Config.FLASK_ENV))
+
+
+app.config['MONGO_URI'] = Config.DB
+# app.register_blueprint(client_bp)
+mongo = PyMongo(app)
+
+# use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
+app.json_encoder = JSONEncoder
+
+
 
 
 @app.route('/')
