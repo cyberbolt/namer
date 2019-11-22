04a4137a61aebaac19442a7003255225f48fc2ce
Bulat Zamilov <bulat.zamilov@gmail.com>
2019-11-22 23:48:41 +0300

    Add security into db part

--- app/config.py
+++ app/config.py
@@ -8,13 +8,17 @@ class Config(object):
     SECRET_KEY = os.getenv('FLASK_SECRET', 'Secret')
     WTF_CSRF_ENABLED = False
 
-    __DB = os.getenv('M_DB', 'namer')
+    __DB = os.getenv('M_DB', 'DB')
     __HOST = os.getenv('M_HOST', 'localhost')
     __PORT = os.getenv('M_PORT', '27017')
+    __USERNAME = os.getenv('M_USERNAME')
+    __PASSWORD = os.getenv('M_PASSWORD')
 
     MONGODB_SETTINGS = {
         'db': __DB,
         'host': __HOST,
         'port': int(__PORT),
+        'username': __USERNAME,
+        'password': __PASSWORD,
         'alias': 'default'
     }
